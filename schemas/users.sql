0219ddcc309dad302f2803743d4c53c4ccee4f0b
jerryan999 <jerryan1989@gmail.com>
2022-07-06 00:04:42 +0800

    remove useless

--- schemas/users.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS Users (
-    id BIGSERIAL PRIMARY KEY,
-    firstName TEXT,
-    lastName TEXT,
-    mobile TEXT,
-    email TEXT UNIQUE,
-    createdAt timestamptz DEFAULT now(),
-    updatedAt timestamptz DEFAULT now()
-);
